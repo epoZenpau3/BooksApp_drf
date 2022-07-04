89f5e1e7cd1549b8b4d6021461900239f0f5573b
npau3 <68152279+npau3@users.noreply.github.com>
2022-07-04 18:13:41 +0200

    feat: add viewsets, change urls

--- iosbackdrf/firstapp/urls.py
+++ iosbackdrf/firstapp/urls.py
@@ -4,14 +4,14 @@ from django.urls import include, path
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register(r'books', views.BookApiView)
+router.register(r'books', views.BookViewSet)
 
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('book/', views.BookApiView.as_view()),
-    path('book/create/', views.BookApiCreateView.as_view()),
-    path('book/detail/<int:pk>/', views.BookApiDetailView.as_view()),
-    path('author/', views.AuthorApiView.as_view()),
-    path('author/detail/<int:pk>/', views.AuthorApiDetailView.as_view()),
+    path('book/', views.BookViewSet.as_view({'get': 'list'})),
+    path('book/detail/<int:pk>/', views.BookViewSet.as_view({'put': 'update', 'get': 'retrieve'})),
+    path('book/create/', views.AuthorViewSet.as_view({'post': 'create'})),
+    path('author/', views.AuthorViewSet.as_view({'get': 'list'})),
+    path('author/detail/<int:pk>/', views.AuthorViewSet.as_view({'post': 'create'})),
 ]
--- iosbackdrf/firstapp/views.py
+++ iosbackdrf/firstapp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .serializers import BookSerializer, BookCreateSerializer
 from .models import Books, Author
 
@@ -8,25 +8,25 @@ def index(request):
     return HttpResponse("Я тут был и создал, но не добавил.")
 
 
-class BookApiView(generics.ListAPIView):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Books.objects.all()
     serializer_class = BookSerializer
 
 
-class BookApiDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Books.objects.all()
-    serializer_class = BookSerializer
-
-
-class BookApiCreateView(generics.CreateAPIView):
-    serializer_class = BookCreateSerializer
-
-
-class AuthorApiView(generics.ListAPIView):
+class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = BookCreateSerializer
 
 
-class AuthorApiDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Author.objects.all()
-    serializer_class = BookCreateSerializer
+# class BookApiCreateView(generics.CreateAPIView):
+#     serializer_class = BookCreateSerializer
+#
+#
+# class AuthorApiView(generics.ListAPIView):
+#     queryset = Author.objects.all()
+#     serializer_class = BookCreateSerializer
+#
+#
+# class AuthorApiDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Author.objects.all()
+#     serializer_class = BookCreateSerializer
